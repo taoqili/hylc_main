79271c10c3bc40be228b98c66bd55316c148c9c0
陶启立 <taoqili@boulderaitech.com>
2022-10-13 17:51:35 +0800

    init

--- src/pages/Home/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default () => {
-  return (
-    <div>
-      <h2>Home page</h2>
-    </div>
-  );
-}
--- src/utils/request.ts
+++
@@ -1,6 +0,0 @@
-import axios from "axios"
-
-export default {
-  get: axios.get,
-  post: axios.post
-}
